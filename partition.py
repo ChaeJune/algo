fc7098a354fbad5a3c4f73bba51ffd91d8b55bb8
ChaeJune <39612618+ChaeJune@users.noreply.github.com>
2025-01-26 13:18:35 +0900

    Update partition.py
    시간복잡도 O(n^3) 에서 O(n^2)으로 줄임

--- partition.py
+++ partition.py
@@ -3,12 +3,12 @@ import sys
 #st = "abbacbcab"
 #st = "BBCDDEDD"
 #st = "AAAA"
-#st ="QWERTYTREWQWERT"[::-1]
+st ="QWERTYTREWQWERT"[::-1]
 #st ="QWERTYTREWQWERTREWQ"
 #st = "ABCDEFGH"
-st = "BBCDDECAECBDABADDCEBACCCBDCAABDBADBCDDECAECBDABADDCEBACCCBDCABCDDECAECBDABADDCEBACCCBDCABCDDECAECBDABADDCEBACCCBDCABCDDECAECBDABADDCEBACCCBDCABCDDECAECBDABADDCEBACCCBDCABCDDECAECBDABADDCEBACCCBDCABCDDECAECBDABADDCEBACCCBDCABCDDECAECBDABADDCEBACCCBDCABCDDECAECBDABADDCEBACCCBDCABCDDECAECBDABADDCEBACCCBDCABCDDECAECBDABADDCEBACCCBDCABCDDECAECBDABADDCEBACCCBDCABCDDECAECBDABADDCEBACCCBDCABCDDECAECBDABADDCEBACCCBDCADBBCDDECAECBDABADDCEBACCCBDCAABDBBBCDDECAECBDABADDCEBACCCBDCAABDBBBCDDECAECBDABADDCEBACCCBDCAABDB"
+#st = "BBCDDECAECBDABADDCEBACCCBDCAABDBADBCDDECAECBDABADDCEBACCCBDCABCDDECAECBDABADDCEBACCCBDCABCDDECAECBDABADDCEBACCCBDCABCDDECAECBDABADDCEBACCCBDCABCDDECAECBDABADDCEBACCCBDCABCDDECAECBDABADDCEBACCCBDCABCDDECAECBDABADDCEBACCCBDCABCDDECAECBDABADDCEBACCCBDCABCDDECAECBDABADDCEBACCCBDCABCDDECAECBDABADDCEBACCCBDCABCDDECAECBDABADDCEBACCCBDCABCDDECAECBDABADDCEBACCCBDCABCDDECAECBDABADDCEBACCCBDCABCDDECAECBDABADDCEBACCCBDCADBBCDDECAECBDABADDCEBACCCBDCAABDBBBCDDECAECBDABADDCEBACCCBDCAABDBBBCDDECAECBDABADDCEBACCCBDCAABDB"
 #print(len(st))
-#st = input()
+st = input()
 def chk(s, left, right, memo):
     while left < right:
         if memo[right][left]:
@@ -26,16 +26,24 @@ dp = [[0]*l for i in range(l)]
 
 for i in range(len(st)):
     width = l-i
+
     for j in range(i):
         #print(j, width+j)
         tf = chk(st, j, j+width, tf_arr)
         tf_arr[j+width][j] = tf
         dp[j+width][j] = 1 if tf else 0
 
+        #if tf:
+        #    print(j,j+width, tf, end ='')
+        #else:
+        #    print(j,j+width, tf, end ='')
+
 for i in range(len(st)):
     tf_arr[i][i] = True
     dp[i][i] = 1      
 
+
+"""
 for i in range(1,l+1):  
     row = i
     col = 0
@@ -54,8 +62,32 @@ for i in range(1,l+1):
             #길이 4인 문자열 1+3 2+2 3+1로 분할
             #dp[3][0] =min dp[2][0] +dp[3][3] ,  dp[0][0] + dp[3][1], dp[1][0]+dp[3][2]
             #print("hi:" , i)
+        
         row += 1
         col += 1
+"""
+
+'''
+for i in range(l):
+    for j in range(l):
+        print(dp[j][i], end = ' ')
+    print("")
+print("=====================")
+'''
+
+
+for right in range(1,l):
+    if not tf_arr[right][0]:
+        dp[right][0] = right+1
+    # dp[1][0] = min(dp[1][0], dp[0][0]+1)
+    # dp[1][0] = min(dp[1][0], dp[1][1]+1)
+    # when tf_arr[2][2] is true
+    # dp[2][0] = min(dp[2][0], dp[1][0]+1)
+    for left in range(1, right+1):
+        if tf_arr[right][left]:
+            dp[right][0] = min(dp[right][0], dp[left-1][0]+1)
+
+
  
 #print(dp)
 sys.stdout.write(str(dp[l-1][0]))
