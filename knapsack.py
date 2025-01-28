0565879085564a229e81d53805a176da68e1738c
ChaeJune <39612618+ChaeJune@users.noreply.github.com>
2025-01-28 20:24:07 +0900

    Update knapsack.py
    complete omitted candidate[0]

--- knapsack.py
+++ knapsack.py
@@ -19,21 +19,25 @@ def process(candidate, cap):
 
 candidate = []
 
-dp = [[0] * (cap+1) for _ in range(N)]
+dp = [[0] * (cap+1) for _ in range(N+1)]
 
 for i in range(N):
     w, v= map(int, input().split())
     candidate.append((w, v))
 
-for i in range(1,N):
+for weight in range(cap, -1, -1):
+    cur_w, cur_v = candidate[0]
+    if cur_w < weight:
+        dp[1][weight-cur_w] = cur_v
+
+#candidate indexing 잘못했음
+for i in range(2, N+1):
     for weight in range(cap, -1, -1):
-        cur_w, cur_v = candidate[i]
-        if cur_w > weight and dp[i][weight] != -1:
+        cur_w, cur_v = candidate[i-1]
+        if cur_w > weight:
             dp[i][weight] = dp[i-1][weight]
-        elif dp[i][weight] != -1 :
-            dp[i][weight] = max(dp[i-1][weight-cur_w] + cur_v , dp[i-1][weight])
         else:
-            pass
+            dp[i][weight] = max(dp[i-1][weight-cur_w] + cur_v , dp[i-1][weight])
 print(dp[N-1][cap])
 
 #tp = candidate
@@ -54,7 +58,7 @@ print(dp[N-1][cap])
 # 가능한 모든 조합을 시도해본다? XOXXO 이런식으로 배열을 정리해서 => N개 있다고 하면 N!을 다 검사해야함 좀 아닌듯
 # dp 배열을 만들어서 capacity 안에 담을 수 있으면 capacity - weight 해서 새로운 주머니의 capacity w를 만듦.
 # i 번째 원소가 새로운 capacity c에 대해서 c-w_i < 0 인 경우에는 그냥 그 다음으로 넘어갈 수밖에 없음
-# i 번째 원소가 새로운 capacity c에 대해서 c-w_i > 0 인 경우에는 v_i 가 i번째 물건의 가치라고 했을 때, max(dp[i-1][cap], v_i + dp[i-1][cap-w_i]) 로 dp[i][cap] 을 갱신
+# i 번째 원소가 새로운 capacity c에 대해서 c-w_i > 0 인 경우에는 v_i 가 i번째 물건의 가치라고 했을 때, max(dp[i-1][cap], v_i + dp[i-1][cap-w_i])
 # i = 0 이거나 cap 이 0인 경우 => 그냥 0임..
 # dp 배열
 # cap을 넘었는지를 언제 체크하는지가 관건일 것 같다.
