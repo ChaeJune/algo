d03edace13c5a5620581bc3ecc7b20cb3c00657a
ChaeJune <39612618+ChaeJune@users.noreply.github.com>
2025-01-28 23:24:11 +0900

    Create dfs_connected.py
    백준문제2667번

---
+++ dfs_connected.py
@@ -0,0 +1,43 @@
+import sys
+sys.setrecursionlimit(10 ** 6)
+n = int(input())
+
+graph = {}
+
+map = []
+
+for i in range(n):
+    arr = list(input())  
+    map.append(arr)
+
+
+def search(x, y, m):
+    rows = n
+    cols = n
+
+    if x < 0 or x >= rows or y < 0 or y >= cols or m[y][x] == '0':
+        return 0
+
+
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    count = 0
+    if m[y][x] == '1':
+        count = 1
+        m[y][x]='0'
+    else:
+        print('hi')
+        return 0
+    for dr, dc in directions:
+        count += search(x + dr, y + dc, m)
+    return count
+
+
+ans = []
+for i in range(n):
+    for j in range(n):
+        if map[i][j] != '0':
+            num = search(j, i, map)
+            ans.append(num)
+final = sorted(ans)
+for item in final:
+    print(item)
