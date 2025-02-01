120066a40bddeb89a2c00619d15871fc2de6bdf1
ChaeJune <39612618+ChaeJune@users.noreply.github.com>
2025-02-01 21:44:40 +0900

    Update hideandseek.py
    visited 추가됨.
    그리고 lambda expression 이용하려고 case 나눔

--- hideandseek.py
+++ hideandseek.py
@@ -6,15 +6,27 @@ a, b= map(int, input().split())
 def bfs_search(x, goal):
 
     oper = [lambda x: x+1, lambda x: x-1, lambda x: x*2]
+    #oper_p = [lambda x: x+1, lambda x: x*2]
+    oper_n = [lambda x: x-1]
     queue = deque([(x, 1)])
+    visited = set()
     while queue:
         curr_x, depth = queue.popleft()
         if curr_x == goal:
             return depth
-        for op in oper:
-            new_x = op(curr_x)
-            if x >= 0 and x<=100000:
-                queue.append((new_x ,depth + 1))
+        if curr_x in visited:
+            continue
+        visited.add(curr_x)
+        if curr_x < goal:
+            for op in oper:
+                new_x = op(curr_x)
+                if x >= 0 and x<=100000:
+                    queue.append((new_x ,depth + 1))
+        else:
+            for op in oper_n:
+                new_x = op(curr_x)
+                if x >= 0 and x<=100000:
+                    queue.append((new_x ,depth + 1))
     return -1
     
 ans = bfs_search(a, b)
